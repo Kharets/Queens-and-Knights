250ebb469fa26d49ae5db2cf1c507fb86667a372
Kharets <71780457+Kharets@users.noreply.github.com>
2023-04-10 22:58:12 +0300

    1

--- Шахматы/Form1.Designer.cs
+++ Шахматы/Form1.Designer.cs
@@ -36,7 +36,20 @@ namespace Шахматы
             this.textBoxY = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -46,20 +59,20 @@ namespace Шахматы
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.ColumnHeadersVisible = false;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(6, 6);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersVisible = false;
-            this.dataGridView1.Size = new System.Drawing.Size(426, 426);
+            this.dataGridView1.Size = new System.Drawing.Size(414, 414);
             this.dataGridView1.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Gray;
             this.button1.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(444, 124);
+            this.button1.Location = new System.Drawing.Point(440, 100);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(249, 50);
+            this.button1.Size = new System.Drawing.Size(250, 50);
             this.button1.TabIndex = 1;
             this.button1.Text = "Ферзи";
             this.button1.UseVisualStyleBackColor = false;
@@ -69,9 +82,9 @@ namespace Шахматы
             // 
             this.button2.BackColor = System.Drawing.Color.Gray;
             this.button2.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, System.Drawing.FontStyle.Bold);
-            this.button2.Location = new System.Drawing.Point(444, 180);
+            this.button2.Location = new System.Drawing.Point(440, 156);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(249, 50);
+            this.button2.Size = new System.Drawing.Size(250, 50);
             this.button2.TabIndex = 2;
             this.button2.Text = "Кони";
             this.button2.UseVisualStyleBackColor = false;
@@ -80,7 +93,7 @@ namespace Шахматы
             // textBoxX
             // 
             this.textBoxX.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBoxX.Location = new System.Drawing.Point(492, 59);
+            this.textBoxX.Location = new System.Drawing.Point(488, 37);
             this.textBoxX.Name = "textBoxX";
             this.textBoxX.Size = new System.Drawing.Size(50, 26);
             this.textBoxX.TabIndex = 3;
@@ -91,7 +104,7 @@ namespace Шахматы
             // textBoxY
             // 
             this.textBoxY.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBoxY.Location = new System.Drawing.Point(593, 60);
+            this.textBoxY.Location = new System.Drawing.Point(589, 37);
             this.textBoxY.Name = "textBoxY";
             this.textBoxY.Size = new System.Drawing.Size(50, 26);
             this.textBoxY.TabIndex = 4;
@@ -102,7 +115,7 @@ namespace Шахматы
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(548, 60);
+            this.label1.Location = new System.Drawing.Point(544, 38);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(39, 20);
             this.label1.TabIndex = 5;
@@ -113,32 +126,158 @@ namespace Шахматы
             // 
             this.button3.BackColor = System.Drawing.Color.Gray;
             this.button3.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, System.Drawing.FontStyle.Bold);
-            this.button3.Location = new System.Drawing.Point(444, 236);
+            this.button3.Location = new System.Drawing.Point(440, 212);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(249, 50);
+            this.button3.Size = new System.Drawing.Size(250, 50);
             this.button3.TabIndex = 6;
             this.button3.Text = "Лабиринт";
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(706, 453);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Teal;
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.textBoxY);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.textBoxX);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(698, 427);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Вкладка 1";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.Teal;
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.button9);
+            this.tabPage2.Controls.Add(this.button8);
+            this.tabPage2.Controls.Add(this.button7);
+            this.tabPage2.Controls.Add(this.button6);
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(698, 427);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Вкладка 2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(6, 6);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(414, 414);
+            this.textBox1.TabIndex = 8;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Gray;
+            this.button9.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(440, 324);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(250, 50);
+            this.button9.TabIndex = 7;
+            this.button9.Text = "6";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Gray;
+            this.button8.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(440, 268);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(250, 50);
+            this.button8.TabIndex = 6;
+            this.button8.Text = "5";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Gray;
+            this.button7.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(440, 212);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(250, 50);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "Пересадки";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Gray;
+            this.button6.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(440, 156);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(250, 50);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "Паркет";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Gray;
+            this.button5.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(440, 100);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(250, 50);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Подстрока";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Gray;
+            this.button4.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(440, 44);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(250, 50);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Матрицы";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Teal;
-            this.ClientSize = new System.Drawing.Size(705, 450);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBoxY);
-            this.Controls.Add(this.textBoxX);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(706, 453);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -151,6 +290,16 @@ namespace Шахматы
         private System.Windows.Forms.TextBox textBoxY;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Шахматы/Form1.cs
+++ Шахматы/Form1.cs
@@ -47,6 +47,17 @@ namespace Шахматы
             InitializeComponent();
         }
 
+        private void textBoxX_TextChanged(object sender, EventArgs e)
+        {
+            try { textBoxY.Text = textBoxX.Text; }
+            catch { }
+        }
+        private void textBoxY_TextChanged(object sender, EventArgs e)
+        {
+            try { textBoxX.Text = textBoxY.Text; }
+            catch { }
+        }
+
         private void input()
         {
             try
@@ -269,16 +280,275 @@ namespace Шахматы
         }
 
 
+        //матрицы
+        private const int count_matrix = 4;
 
-        private void textBoxX_TextChanged(object sender, EventArgs e)
+        private void button4_Click(object sender, EventArgs e)
         {
-            try { textBoxY.Text = textBoxX.Text; }
-            catch { }
+            textBox1.Text = "";
+
+            int[,] matrix = new int [count_matrix, 2] { { 10, 20}, { 20, 50}, { 50, 40}, { 40, 20} };
+            int[,] table = new int [count_matrix - 1, count_matrix - 1];
+            for (int i = 0; i < count_matrix - 1; i++)
+            {
+                for (int j = 0; j < count_matrix - 1 - i; j++)
+                {
+                    if (i == 0)
+                    {
+                        table[0, j] = matrix[j,0] * matrix[j,1] * matrix[j + 1,1];
+                    }
+                    else
+                    {
+                        if (table[i - 1,j] <= table[i - 1,j + 1])
+                        {
+                            table[i,j] = table[i - 1,j] + (matrix[j,0] * matrix[i + 1 + j,0] * matrix[i + 1 + j,1]);
+                        }
+                        else
+                        {
+                            table[i,j] = table[i - 1,j + 1] + (matrix[j,0] * matrix[j,1] * matrix[i + 1 + j,1]);
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < count_matrix - 1; i++)
+            {
+                for (int j = 0; j < count_matrix - 1; j++)
+                {
+                    textBox1.Text += table[i,j].ToString() + " ";
+                }
+                textBox1.Text += "\r\n";
+            }
+            textBox1.Text += "Return: " + table[count_matrix - 2, 0].ToString();
+
+        }  
+        //подстрока
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+
+            string s1 = "nhfycajhvf";
+            string s2 = "qktrnhjcnfywbz";
+
+            char[] str1 = s1.ToCharArray();
+            char[] str2 = s2.ToCharArray();
+            int[,] table = new int [str1.Length, str2.Length];
+
+            if (str1[0] == str2[0])
+            {
+                table[0,0] = 1;
+            }
+            else
+            {
+                table[0,0] = 0;
+            }
+            for (int i = 0; i < str1.Length; i++)
+            {
+                for (int j = 0; j < str2.Length; j++)
+                {
+                    if (i == 0 && j > 0)
+                    {
+                        if (str1[0] == str2[j])
+                        {
+                            if (str1[0] == str2[j - 1])
+                            {
+                                table[0,j] = table[0,j - 1] + 1;
+                            }
+                            else if (table[0,j - 1] > 1)
+                            {
+                                table[0,j] = table[0,j - 1];
+                            }
+                            else
+                            {
+                                table[0,j] = 1;
+                            }
+                        }
+                        else
+                        {
+                            table[0,j] = table[0,j - 1];
+                        }
+                    }
+                    else
+                    {
+                        if (i != 0 && j == 0 && str1[i] != str2[0])
+                        {
+                            table[i, 0] = table[i - 1, 0];
+                        }
+                        else if (i != 0 && j != 0 && str1[i] != str2[j])
+                        {
+                            if (table[i,j - 1] > table[i - 1,j])
+                            {
+                                table[i,j] = table[i,j - 1];
+                            }
+                            else
+                            {
+                                table[i,j] = table[i - 1,j];
+                            }
+                        }
+                        else if(i != 0 && j != 0)
+                        {
+                            if (table[i,j - 1] > table[i - 1,j])
+                            {
+                                table[i,j] = table[i,j - 1] + 1;
+                            }
+                            else
+                            {
+                                table[i,j] = table[i - 1,j] + 1;
+                            }
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < str1.Length; i++)
+            {
+                for (int j = 0; j < str2.Length; j++)
+                {
+                    textBox1.Text += table[i, j].ToString() + " ";
+                }
+                textBox1.Text += "\r\n";
+            }
+             
+            int a = table[str1.Length - 1, str2.Length - 1];
+
+            textBox1.Text += "Result: " + a;
+
         }
-        private void textBoxY_TextChanged(object sender, EventArgs e)
+
+        //паркет
+
+        private bool good(int mask)
         {
-            try { textBoxX.Text = textBoxY.Text; }
-            catch { }
+            int pos = NP - 1;
+            while (pos >= 0)
+                if ((~mask & pow2[pos]) != 0)
+                    if (pos == 0)
+                        return false;
+                    else if ((mask & pow2[pos - 1]) != 0)
+                        return false;
+                    else pos -= 2;
+                else pos--;
+            return true;
+        }
+
+        private long[,] Res = new long[21, 256];
+        private int[] goodMask = new int[256];
+        private int[] pow2 = new int[8];
+
+        private int NP = 8, MP = 20;        
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            textBox1.Text = "";
+
+
+            for (int i = 0; i < NP; i++)
+                pow2[i] = 1 << i;
+            int border = (1 << NP);
+            for (int i = 0; i < border; i++)
+            {
+                if (good(i))
+                    goodMask[i] = 1;
+                else
+                    goodMask[i] = 0;
+            }
+            Res[0,0] = 1;
+            for (int pos = 0; pos < MP; pos++)
+                for (int i = 0; i < border; i++)
+                    for (int j = 0; j < border; j++)
+                        if ((i & j) == 0)
+                            if (goodMask[i | j] != 0)
+                                Res[pos + 1,j] += Res[pos,i];
+
+            for (int i = 0; i <= MP; i++)
+                textBox1.Text += Res[i, 0] + " ";
+
+            long a = Res[MP, 0];
+
+            textBox1.Text += "\r\nResult: " + a;
+
+
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+
+            Random rand = new Random();
+
+            int costMax = 300;
+            int costMin = 100;
+
+            int gap = 100;
+
+            int[] stations = new int[rand.Next(6, 10)];            
+
+            int[] selectedTransfer = new int[stations.Length];
+
+            int price = 0;
+
+            double s = 0;
+
+            for (int i = 0; i < stations.Length; i++)
+                stations[i] = rand.Next(costMin, costMax);
+
+            int[] stationsTotalCost = stations.Clone() as int[];
+
+            for (int i = 1; i < stations.Length; i++)
+            {
+                stationsTotalCost[i] += stationsTotalCost[i - 1];
+            }
+
+            for (int i = 0; i < stations.Length; i++)
+            {
+                s += stations[i];
+
+                if (Math.Round(s / gap) * gap < s || i == stations.Length - 1)
+                {
+                    price += Convert.ToInt32(Math.Round(s / gap) * gap);
+                    selectedTransfer[i] = Convert.ToInt32(Math.Round(s / gap) * gap);
+                    s = 0;
+                }
+
+            }
+
+            textBox1.Text += "Стоимость между станциями: ";
+
+            for (int i = 0; i < stations.Length; i++)
+            {
+                textBox1.Text += " " + stations[i];
+            }
+
+            textBox1.Text += "\r\nСтоимость между станциями, если платить за каждую, без округления: ";
+
+            for (int i = 0; i < stations.Length; i++)
+            {
+                textBox1.Text += " " + stationsTotalCost[i];
+            }
+
+            textBox1.Text += "\r\nВыбранные нами станции, и их стоимость: ";
+
+            for (int i = 0; i < stations.Length; i++)
+            {
+                textBox1.Text += " " + selectedTransfer[i];
+            }
+
+            textBox1.Text += "\r\nОбщая стоимость выбранных нами станций: ";
+
+            textBox1.Text += " " + price;
+
+
+
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+
         }
 
     }
